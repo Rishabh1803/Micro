36d02f4add680e1ab4407d9761f6fda17bd1359d
Rishabh <31581762+Rishabh1803@users.noreply.github.com>
2022-12-24 00:59:12 +0530

    Hotel Service

---
+++ HotelService/src/main/java/com/rk/hotel/service/services/HotelService.java
@@ -0,0 +1,12 @@
+package com.rk.hotel.service.services;
+
+import com.rk.hotel.service.entities.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    Hotel create(Hotel hotel);
+    List<Hotel> getAll();
+    Hotel get(String id);
+}
---
+++ HotelService/src/main/java/com/rk/hotel/service/services/impl/HotelServiceImpl.java
@@ -0,0 +1,34 @@
+package com.rk.hotel.service.services.impl;
+
+import com.rk.hotel.service.entities.Hotel;
+import com.rk.hotel.service.exceptions.ResourceNotFoundException;
+import com.rk.hotel.service.repositories.HotelRepository;
+import com.rk.hotel.service.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Override
+    public Hotel create(Hotel hotel) {
+        hotel.setId(UUID.randomUUID().toString());
+        return hotelRepository.save(hotel);
+    }
+
+    @Override
+    public List<Hotel> getAll() {
+        return hotelRepository.findAll();
+    }
+
+    @Override
+    public Hotel get(String id) {
+        return hotelRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Hotel with given id not found: " + id));
+    }
+}
